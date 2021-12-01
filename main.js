3da544e93d010edcc18258277ea3fa3a605d2295
napple29 <snwxxx29@gmail.com>
2021-12-01 12:57:14 +0900

    メモの追加・削除機能を実装

---
+++ main.js
@@ -0,0 +1,47 @@
+const STORAGE_KEY = 'todos-vuejs-2.0';
+const todoStorage = {
+  fetch() {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach((todo, index) =>
+      todo.id = index
+    );
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+}
+
+const app = new Vue({
+  data: {
+    todos: todoStorage.fetch(),
+    newTodo: ''
+  },
+  computed: {
+    filteredTodos() {
+      return this.todos;
+    }
+  },
+
+  methods: {
+    addTodo() {
+      const value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      });
+      this.newTodo = '';
+      todoStorage.save(this.todos);
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+      todoStorage.save(this.todos);
+    }
+  }
+})
+app.$mount('.todoapp');
