bc317898db5aa8d226812089d686423bea93e73a
napple29 <snwxxx29@gmail.com>
2021-12-01 14:05:57 +0900

    メモをダブルクリックで選択すると編集フォームが出るよう実装

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
-const STORAGE_KEY = 'todos-vuejs-2.0';
+const STORAGE_KEY = 'todos-vuejs-2.0'
 const todoStorage = {
   fetch() {
-    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
     todos.forEach((todo, index) =>
       todo.id = index
-    );
-    todoStorage.uid = todos.length;
-    return todos;
+    )
+    todoStorage.uid = todos.length
+    return todos
   },
   save(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
 
 const app = new Vue({
   data: {
     todos: todoStorage.fetch(),
+    editedTodo: null,
     newTodo: ''
   },
   computed: {
     filteredTodos() {
-      return this.todos;
+      return this.todos
     }
   },
 
   methods: {
     addTodo() {
-      const value = this.newTodo && this.newTodo.trim();
+      const value = this.newTodo && this.newTodo.trim()
       if (!value) {
-        return;
+        return
       }
       this.todos.push({
         id: todoStorage.uid++,
         title: value,
         completed: false
-      });
-      this.newTodo = '';
-      todoStorage.save(this.todos);
+      })
+      this.newTodo = ''
+      todoStorage.save(this.todos)
     },
+    editTodo(todo) {
+			this.editedTodo = todo
+		},
     removeTodo(todo) {
-      this.todos.splice(this.todos.indexOf(todo), 1);
-      todoStorage.save(this.todos);
+      this.todos.splice(this.todos.indexOf(todo), 1)
+      todoStorage.save(this.todos)
     }
   }
 })
-app.$mount('.todoapp');
+app.$mount('.todoapp')
